9ca0fdbda6d672cf820bb379b4ebdfbab7685712
tiomacaco <omer@otela.homes>
2026-06-21 00:19:44 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-chown -R 10000:10000 /opt/hermes/.hermes
+echo "ENTRYPOINT: chowning disk"
+chown -R 10000:10000 /opt/hermes/.hermes || echo "chown failed"
+ls -la /opt/hermes | grep hermes
 exec /init /opt/hermes/docker/main-wrapper.sh "$@"
